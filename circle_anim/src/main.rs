c8e4ca66fe63b8231d9850f67bcff7e4507a1261
5Daydreams <nelsonkossuga@gmail.com>
2022-10-19 01:54:59 +0200

    renaming into rust conventions;

---
+++ circle_anim/src/main.rs
@@ -0,0 +1,29 @@
+// nannou provides this thing called a prelude to help with handling reusage of code from other scripts;
+use nannou::prelude::{rgb::Rgb, *};
+// Interestingly enough, rgb::Rgb was not added by me! VSCode does that Rider-thing where it checks for usage and includes the libs/files
+
+fn main() {
+    nannou::app(model).update(update).simple_window(view).run();
+}
+
+struct Model {}
+
+fn model(_app: &App) -> Model {
+    Model {}
+}
+
+fn update(_app: &App, _model: &mut Model, _update: Update) {}
+
+fn view(_app: &App, _model: &Model, frame: Frame) {
+    // Prepare to draw.
+    let draw = _app.draw();
+
+    let bg_color: Rgb = Rgb::new(0.25, 0.05, 0.45);
+    // Clear the background to purple.
+    draw.background().color(bg_color);
+
+    // Draw a blue ellipse with a radius of 10 at the (x,y) coordinates of (0.0, 0.0)
+    draw.ellipse().color(STEELBLUE).x_y(0.0, 0.0);
+
+    draw.to_frame(_app, &frame).unwrap();
+}
