117de79e43c1b7abe0061b39e0dbcf5bf1ffa32d
5Daydreams <nelsonkossuga@gmail.com>
2022-10-23 15:22:46 +0200

    Growing circles are currently working;

--- nannou_helloWorld/src/main.rs
+++ nannou_helloWorld/src/main.rs
@@ -1,41 +1,196 @@
-// nannou provides this thing called a prelude to help with handling reusage of code from other scripts;
-use nannou::prelude::{rgb::Rgb, *};
+use nannou::prelude::*;
+// nannou provides this thing called a prelude to help with
+// handling reusage of code from other scripts;
+use rand::prelude::*;
 
-fn main() {
-    // sketches take no input/output, apps are more complex
-    // nannou::sketch(view).run();
-    nannou::app(model).update(update).simple_window(view).run();
+use structopt::StructOpt;
+
+use lazy_static::lazy_static;
+
+lazy_static! {
+	pub static ref OPT: Opt = Opt::from_args();
 }
 
-struct Model {}
-
-fn model(_app: &App) -> Model {
-    Model {}
+/// A nannou demonstration application
+#[derive(StructOpt, Debug)]
+#[structopt(name = "nannou_dots")]
+pub struct Opt
+{
+	/// Set dot growth rate
+	#[structopt(short, long, default_value = "1.0")]
+	rate: f32,
 }
 
-fn update(_app: &App, _model: &mut Model, _update: Update) {}
+fn main()
+{
+	// sketches take no input/output, apps are more
+	// complex nannou::sketch(view).run();
+	nannou::app(model)
+		.update(update)
+		.run();
+}
 
-fn view(_app: &App, _model: &Model, frame: Frame) {
-    // get canvas to draw on
-    let draw: Draw = _app.draw();
+/// A coordinate pair - the (0,0) default is the center of the frame
+#[derive(Debug, Default, Clone, Copy)]
+struct Point
+{
+	x: f32,
+	y: f32,
+}
 
-    let bg_color: Rgb = Rgb::new(0.15, 0.05, 0.4);
-    draw.background().color(bg_color);
+// Constructor method for the Point struct
+impl Point
+{
+	fn new(x: f32, y: f32) -> Self { Self { x, y } }
+}
 
-    let iteration_count: i32 = 500;
+/// Things that can be drawn to the screen
+trait Nannou
+{
+	fn display(&self, draw: &nannou::Draw);
+	fn update(&mut self);
+}
 
-    let points = (0..iteration_count).map(|i: i32| {
-        let x: f32 = (i - iteration_count / 2) as f32 / 10.0; //subtract 25 to center the sine wave
-        let y: f32 = x.sin();
-        let point: Vec2 = pt2(x, y) * 20.0; //scale sine wave by 20.0
+/// A circle to paint
+#[derive(Debug, Clone, Copy)]
+struct Dot
+{
+	color: Srgba<u8>,
+	origin: Point,
+	radius: f32,
+	max_radius: f32,
+	growth_rate: f32,
+}
 
-        let sine_value: f32 = (y + 1.0) * 0.5;
+impl Dot
+{
+	fn new() -> Self { Self::default().set_growth_rate(OPT.rate) }
+	fn set_growth_rate(mut self, rate: f32) -> Self
+	{
+		self.growth_rate = rate;
+		return self;
+	}
+}
 
-        let sine_color: Rgb = Rgb::new(0.01, 0.2, 0.6 * sine_value);
-        (point, sine_color)
-    });
-    draw.polyline().weight(3.0).points_colored(points);
+impl Nannou for Dot
+{
+	fn display(&self, draw: &nannou::Draw)
+	{
+		draw.ellipse()
+			.w(self.radius)
+			.h(self.radius)
+			.x_y(self.origin.x, self.origin.y)
+			.color(self.color);
+	}
+	fn update(&mut self)
+	{
+		if self.radius < self.max_radius
+		{
+			self.radius += self.growth_rate;
+		}
+		else
+		{
+			let mut rng: ThreadRng = rand::thread_rng();
+			self.radius = 0.0;
 
-    // put everything on the frame
-    draw.to_frame(_app, &frame).unwrap();
+			let x: f32 = rng.gen_range(-250.0 .. 250.0);
+			let y: f32 = rng.gen_range(-250.0 .. 250.0);
+
+			let new_pos: Point = Point::new(x, y);
+			self.origin = Point::new(new_pos.x, new_pos.y);
+		}
+	}
+}
+
+impl Default for Dot
+{
+	fn default() -> Self
+	{
+		Self {
+			color: Srgba::<u8>::new(15,80,180,105),
+			origin: Point::default(),
+			radius: 10.0,
+			max_radius: 200.0,
+			growth_rate: 1.0,
+		}
+	}
+}
+
+#[derive(Debug, Default, Clone, Copy)]
+struct Vec2<T>
+{
+	x: T,
+	y: T,
+}
+
+// Constructor method for the Point struct
+impl Vec2<u32>
+{
+	fn new(x: u32, y: u32) -> Self { Self { x, y } }
+}
+
+/// The application state
+#[derive(Debug)]
+struct Model
+{
+	bg_color: Srgba<u8>,
+	window_size: Vec2<u32>,
+	dot: Dot,
+}
+
+impl Default for Model
+{
+	fn default() -> Self
+	{
+		Self {
+			bg_color: Srgba::<u8>::new(150,140,120,255),
+            window_size: Vec2::<u32>::new(800, 600),
+			dot: Dot::new(),
+		}
+	}
+}
+
+impl Nannou for Model
+{
+	/// Show this model
+	fn display(&self, draw: &nannou::Draw)
+	{
+		draw.background()
+			.color(self.bg_color);
+		self.dot
+			.display(draw);
+	}
+	/// Update this model
+	fn update(&mut self) { self.dot.update(); }
+}
+
+// Nannou interface
+//
+
+/// Nannou app model
+fn model(_app: &App) -> Model
+{
+	let model: Model = Model::default();
+
+	_app.new_window()
+		.size(model.window_size.x, model.window_size.y)
+		.view(view)
+		.build()
+		.unwrap();
+
+	return model;
+}
+
+/// Nannou app update
+fn update(_app: &App, model: &mut Model, _update: Update) { model.update(); }
+
+/// Nannou app view
+fn view(app: &App, model: &Model, frame: Frame)
+{
+	let draw = app.draw();
+	// Draw model
+	model.display(&draw);
+	// Render frame
+	draw.to_frame(&app, &frame)
+		.unwrap();
 }
