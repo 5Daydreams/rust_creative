0f6b196e08d8ebaec861a6d516dd1a06bf063d3c
5daydreams <nelsonkossuga@gmail.com>
2022-10-24 22:04:28 +0200

    Added skewed cossines for the DNA curve;

--- perlin_curve/src/helper_structs.rs
+++ perlin_curve/src/helper_structs.rs
@@ -1,31 +1,32 @@
 use nannou::prelude::*;
 
-pub struct Vector2<T>
-{
+pub struct Vector2<T> {
 	pub x: T,
 	pub y: T,
 }
 
 pub type Vec2Int = Vector2<u32>;
 
-impl Vec2Int
-{
-	pub fn new(x: u32, y: u32) -> Self { Self { x: x, y: y } }
+impl Vec2Int {
+	pub fn new(x: u32, y: u32) -> Self {
+		Self { x: x, y: y }
+	}
 }
 
-pub struct Model
-{
+pub struct Model {
 	pub bg_color: Srgb<f32>,
 	pub window_size: Vec2Int,
 }
 
-impl Model
-{
-	pub fn new(color: Srgb<f32>, size: Vec2Int) -> Self
-	{
+impl Model {
+	pub fn new(color: Srgb<f32>, size: Vec2Int) -> Self {
 		Self {
 			bg_color: color,
 			window_size: size,
 		}
 	}
+}
+
+pub fn skewed_cos(x: f32, skewness: f32) -> f32 {
+	return (x + x.cos() * skewness).cos();
 }
--- perlin_curve/src/main.rs
+++ perlin_curve/src/main.rs
@@ -1,17 +1,14 @@
 mod helper_structs;
 
 use helper_structs::*;
-use nannou::draw::background::new;
-use nannou::noise::{
-	NoiseFn,
-	Perlin,
-};
+// use nannou::noise::{NoiseFn, Perlin};
 use nannou::prelude::*;
 
-fn main() { nannou::app(model).run(); }
+fn main() {
+	nannou::app(model).run();
+}
 
-fn model(_app: &App) -> Model
-{
+fn model(_app: &App) -> Model {
 	let color_value: Srgb<f32> = Srgb::<f32>::new(0.05, 0.1, 0.1);
 	let w_size = Vec2Int::new(800, 600);
 
@@ -26,48 +23,58 @@ fn model(_app: &App) -> Model
 	return model;
 }
 
-fn view(_app: &App, _model: &Model, frame: Frame)
-{
+fn view(_app: &App, _model: &Model, frame: Frame) {
 	let draw: Draw = _app.draw();
 
 	draw.background()
 		.color(_model.bg_color);
 
-	const SPEED: f32 = 2.0;
+	const SPEED: f32 = 150.0;
 	let t: f32 = _app.time * SPEED;
 
 	let width: f32 = 350.;
 	let height: f32 = 150.;
 
-	let sampling_scale: f32 = 5.0;
+	let sampling_scale: f32 = 3.0;
+	let sample_offset: f32 = 1.7;
+	let skewness: f32 = 0.25;
 
-	let step_size: usize = 5;
+	let step_size: usize = 7;
 	let element_count: usize = ((width * 2.0) as usize) / step_size;
-
 	let mut points: Vec<Vec2> = Vec::new();
 
-	let start_pos: f32 = -width;
+	let start_x_value: f32 = -width;
 
-	let sample_value = (start_pos * sampling_scale).cos();
-	let point_pos: Vec2 = Vec2::new(start_pos, height * sample_value);
+	let sample_value: f32 = ((start_x_value + t) * sampling_scale) / width;
+
+	let start_sample_result =
+		helper_structs::skewed_cos(sample_value + sample_offset * 0.5 - PI * 0.5, skewness) * 0.5
+			+ 0.5;
+
+	let point_pos: Vec2 = Vec2::new(
+		start_x_value,
+		height * (sample_value) * (0.5 + 0.5 * start_sample_result),
+	);
 
 	points.push(point_pos);
 
-	for i in 1 .. element_count
-	{
-		// match i % 2 {
-        //     1 => // do top line
-        //     _ => // do bottom line
-        // }
+	for i in 1..element_count {
+		let x_value: f32 = 2.0 * width * (i as f32 / element_count as f32) + start_x_value;
 
-		let sampling_pos: f32 = 2.0 * width * (i as f32 / element_count as f32) + start_pos;
+		let sample_value: f32 = ((x_value + t) * sampling_scale) / width;
 
-		let sample_value: f32 = (sampling_pos * sampling_scale).cos();
-		let perlin_value: f32 = height * sample_value;
+		let sample_result: f32 = match i % 2 {
+			| 0 => helper_structs::skewed_cos(sample_value, skewness), // do top line
+			| _ => helper_structs::skewed_cos(sample_value + sample_offset, skewness), // do bottom line
+		};
 
-		let offset = (1.0 - 2.0 * (i % 2) as f32) * height * 0.5;
+		let y_value: f32 = height * sample_result;
 
-		let curr_vec = Vec2::new(sampling_pos, perlin_value - offset);
+		let normalized_sample_result =
+			helper_structs::skewed_cos(sample_value + sample_offset * 0.5 - PI * 0.5, skewness)
+				* 0.5 + 0.5;
+
+		let curr_vec = Vec2::new(x_value, y_value * (0.5 + 0.5 * normalized_sample_result));
 
 		points.push(curr_vec);
 
@@ -75,8 +82,12 @@ fn view(_app: &App, _model: &Model, frame: Frame)
 			.start(points[i - 1])
 			.end(points[i])
 			.caps_round()
-			.stroke_weight(3.0)
-			.color(Srgb::<f32>::new(0.9, 0.9, 0.9));
+			.stroke_weight(2.0 + 1.5 * normalized_sample_result)
+			.color(Srgb::<f32>::new(
+				normalized_sample_result,
+				normalized_sample_result,
+				normalized_sample_result,
+			));
 	}
 
 	draw.to_frame(_app, &frame)
