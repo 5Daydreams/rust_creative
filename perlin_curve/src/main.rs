377fea0d65060616455f4bcdd174ebf85529275f
5Daydreams <nelsonkossuga@gmail.com>
2022-10-24 02:23:29 +0200

    + Added the moving perlin lines;

--- perlin_curve/src/main.rs
+++ perlin_curve/src/main.rs
@@ -29,27 +29,75 @@ fn view(_app: &App, _model: &Model, frame: Frame)
 {
 	let draw: Draw = _app.draw();
 
-	let t: f32 = _app.time;
-
 	draw.background()
 		.color(_model.bg_color);
 
-	let start_pos: Vec2 = Vec2::new(0.0, 0.0);
+	const SPEED: f64 = 2.0;
+	let t: f64 = (_app.time as f64) * SPEED;
 
-	let radius: f32 = 200.;
-	let cos: f32 = t.cos();
-	let sin: f32 = t.sin();
+	let horizontal: f32 = 300.;
+	let vertical: f32 = 150.;
 
-	let end_pos: Vec2 = Vec2::new(radius * cos, radius * sin);
+	let range: std::ops::RangeInclusive<i32> = -horizontal as i32 ..= horizontal as i32;
 
-	let thing: Perlin = Perlin::new();
-	let point_pos: [f64; 2] = [start_pos.x as f64, start_pos.y as f64];
-	thing.get(point_pos);
+	let step_size: usize = 30;
+	let perlin: Perlin = Perlin::new();
+	let noise_scale: f64 = 5.0;
+	let amplitude_scale = 0.7;
+	let amplitude_offset = 1.0;
 
-	draw.line()
-		.start(start_pos)
-		.end(end_pos)
-		.color(Srgb::<f32>::new(1.0, 1.0, 1.0));
+	let mut _last_end_pos: Option<Vec2>;
+
+	for value in range.step_by(step_size)
+	{
+		let perlin_sample_pos: f64 = (value as f64 / (horizontal as f64)) * noise_scale;
+
+		let point_pos: [f64; 2] = [t + perlin_sample_pos * 1.1, perlin_sample_pos * 0.2];
+		let perlin_value_start: f32 = perlin.get(point_pos) as f32;
+		let start_x_offset: f32 = perlin_value_start * (step_size as f32);
+		let start_pos: Vec2 = Vec2::new(
+			(value as f32) + start_x_offset,
+			vertical * (perlin_value_start * amplitude_scale - amplitude_offset),
+		);
+
+		let point_pos: [f64; 2] = [t + perlin_sample_pos * 0.2, perlin_sample_pos * 1.1];
+		let perlin_value_end: f32 = perlin.get(point_pos) as f32;
+		let end_x_offset: f32 = perlin_value_end * (step_size as f32);
+		let end_pos: Vec2 = Vec2::new(
+			(value as f32) - end_x_offset,
+			vertical * (perlin_value_end * amplitude_scale + amplitude_offset),
+		);
+
+		let perlin_value_delta: f32 =
+			((end_pos.y) - (start_pos.y)) / (2.5 * (amplitude_scale + amplitude_offset) * vertical);
+
+		draw.line()
+			.start(start_pos)
+			.end(end_pos)
+			.caps_round()
+			.stroke_weight(3.0)
+			.color(Srgb::<f32>::new(
+				perlin_value_delta,
+				perlin_value_delta,
+				perlin_value_delta,
+			));
+
+		// if last_end_pos.is_some()
+		// {
+		// 	draw.line()
+		// 		.start(start_pos)
+		// 		.end(last_end_pos.unwrap())
+		// 		.caps_round()
+		// 		.stroke_weight(3.0)
+		// 		.color(Srgb::<f32>::new(
+		// 			perlin_value_delta,
+		// 			perlin_value_delta,
+		// 			perlin_value_delta,
+		// 		));
+		// }
+
+		// last_end_pos = Some(end_pos);
+	}
 
 	draw.to_frame(_app, &frame)
 		.unwrap();
