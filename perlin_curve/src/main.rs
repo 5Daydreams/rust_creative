9380e6d8637a61cb44c385cf2ff0f5b2f70b10bd
5daydreams <nelsonkossuga@gmail.com>
2022-10-25 14:37:16 +0200

    changed several values into constants;

--- perlin_curve/src/main.rs
+++ perlin_curve/src/main.rs
@@ -4,10 +4,11 @@ use helper_structs::*;
 // use nannou::noise::{NoiseFn, Perlin};
 use nannou::prelude::*;
 
-fn main() { nannou::app(model).run(); }
+fn main() {
+	nannou::app(model).run();
+}
 
-fn model(_app: &App) -> Model
-{
+fn model(_app: &App) -> Model {
 	let color_value: Srgb<f32> = Srgb::<f32>::new(0.05, 0.1, 0.1);
 	let w_size = Vec2Int::new(800, 600);
 
@@ -22,61 +23,63 @@ fn model(_app: &App) -> Model
 	model
 }
 
-fn view(_app: &App, _model: &Model, frame: Frame)
-{
+fn view(_app: &App, _model: &Model, frame: Frame) {
 	let draw: Draw = _app.draw();
 
 	draw.background()
 		.color(_model.bg_color);
 
-	const SPEED: f32 = 150.0;
-	let t: f32 = _app.time * SPEED;
+	const SCROLL_SPEED: f32 = 150.0;
+	let t: f32 = _app.time * SCROLL_SPEED;
 
-	let width: f32 = 350.;
-	let height: f32 = 150.;
+	const WIDTH: f32 = 350.;
+	const HEIGHT: f32 = 200.;
 
-	let sampling_scale: f32 = 3.0;
-	let sample_offset: f32 = 1.7;
-	let skewness: f32 = 0.25;
+	const SAMPLING_SCALE: f32 = 3.0;
+	const SAMPLE_OFFSET: f32 = 1.7;
+	const SKEWNESS: f32 = 0.25;
+	const BASE_LERP_VALUE: f32 = 0.4;
+	const MIDPOINT_LERP_VALUE: f32 = 0.4;
 
-	let step_size: usize = 7;
-	let element_count: usize = ((width * 2.0) as usize) / step_size;
+	const STEP_SIZE: usize = 7;
+	const ELEMENT_COUNT: usize = ((WIDTH * 2.0) as usize) / STEP_SIZE;
 	let mut points: Vec<Vec2> = Vec::new();
 
-	let start_x_value: f32 = -width;
+	let start_x_value: f32 = -WIDTH;
 
-	let sample_value: f32 = ((start_x_value + t) * sampling_scale) / width;
+	let sample_value: f32 = ((start_x_value + t) * SAMPLING_SCALE) / WIDTH;
+	let sample_result: f32 = helper_structs::skewed_cos(sample_value, SKEWNESS);
 
-	let start_sample_result =
-		helper_structs::skewed_cos(sample_value + sample_offset * 0.5 - PI * 0.5, skewness) * 0.5
+	let y_value: f32 = HEIGHT * sample_result;
+
+	let normalized_sample_result =
+		helper_structs::skewed_cos(sample_value + SAMPLE_OFFSET * 0.5 - PI * 0.5, SKEWNESS) * 0.5
 			+ 0.5;
 
-	let point_pos: Vec2 = Vec2::new(
+	let curr_vec: Vec2 = Vec2::new(
 		start_x_value,
-		height * (sample_value) * (0.5 + 0.5 * start_sample_result),
+		y_value * (BASE_LERP_VALUE + MIDPOINT_LERP_VALUE * normalized_sample_result),
 	);
 
-	points.push(point_pos);
+	points.push(curr_vec);
 
-	for i in 1 .. element_count
-	{
-		let x_value: f32 = 2.0 * width * (i as f32 / element_count as f32) + start_x_value;
+	for i in 1..ELEMENT_COUNT {
+		let x_value: f32 = 2.0 * WIDTH * (i as f32 / ELEMENT_COUNT as f32) + start_x_value;
 
-		let sample_value: f32 = ((x_value + t) * sampling_scale) / width;
+		let sample_value: f32 = ((x_value + t) * SAMPLING_SCALE) / WIDTH;
 
-		let sample_result: f32 = match i % 2
-		{
-			| 0 => helper_structs::skewed_cos(sample_value, skewness), // do top line
-			| _ => helper_structs::skewed_cos(sample_value + sample_offset, skewness), // do bottom line
+		let sample_result: f32 = match i % 2 {
+			| 0 => helper_structs::skewed_cos(sample_value, SKEWNESS), // do top line
+			| _ => helper_structs::skewed_cos(sample_value + SAMPLE_OFFSET, SKEWNESS), // do bottom line
 		};
 
-		let y_value: f32 = height * sample_result;
+		let y_value: f32 = HEIGHT * sample_result;
 
 		let normalized_sample_result =
-			helper_structs::skewed_cos(sample_value + sample_offset * 0.5 - PI * 0.5, skewness)
+			helper_structs::skewed_cos(sample_value + SAMPLE_OFFSET * 0.5 - PI * 0.5, SKEWNESS)
 				* 0.5 + 0.5;
 
-		let curr_vec = Vec2::new(x_value, y_value * (0.5 + 0.5 * normalized_sample_result));
+		let curr_vec = Vec2::new(x_value, y_value * (BASE_LERP_VALUE + MIDPOINT_LERP_VALUE * normalized_sample_result));
 
 		points.push(curr_vec);
 
